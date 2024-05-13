bcd7f8650b30cc91e8ac59b30ab4a83e843da321
Sheheryar Sewani <shey@shey.ca>
2024-05-13 18:44:51 -0400

    Pay Tables (#3)
    * created pay tables
    
    * default to stripe
    
    * explainer
    
    * some clean up

--- README.md
+++ README.md
@@ -3,34 +3,37 @@
 ## Pay, Stripe and Rails Subscription Billing Sample App
 
 
-#### Installation and Configuration
+### Installation and Configuration
 1. config/initializers/stripe.rb
 1. config/initializers/pay.rb
 1. app/models/user.rb
 
-#### Plans and Prices
+### Pay Tables and The Work You Don't Have To Do
+#### bin/rails pay:install:migrations
+* pay_customers
+* pay_subscriptions
+* pay_charges
+* pay_webhooks
+* pay_merchants
+* pay_payment_methods
+
+#### payment_processor
+By including pay_customer in the User model, the gem internally associates a User with the pay_customer model via the owner_type and owner_id fields.
+
+### Plans and Prices
 1. app/models/plan.rb
 
-#### The One (and a half) Controller
+### The One (and a half) Controller
 app/controllers/checkouts_controller.rb
 
-#### Success, Redirects, and Syncing
+### Success, Redirects, and Syncing
 
-#### The One Webhook
+### The One Webhook
 app/services/payment_succeded_handler.rb
 
-#### The Work You Don't Have To Do
-1. Data Modelling
-
-#### Related Articles
-https://gist.github.com/shey/0dd302d762682c06f91d49b5417cb8f5
-
-https://github.com/pay-rails/pay/blob/main/docs/1_installation.md
-
-https://github.com/pay-rails/pay/blob/main/docs/1_installation.md
-
-https://github.com/pay-rails/pay/blob/main/docs/stripe/8_stripe_checkout.md#how-to-use-stripe-checkout-with-pay
-
-https://github.com/pay-rails/pay/blob/main/docs/7_webhooks.md
-
-https://github.com/pay-rails/pay/blob/main/docs/stripe/5_webhooks.md
+### Related Articles
+1. https://github.com/pay-rails/pay/blob/main/docs/1_installation.md
+1. https://github.com/pay-rails/pay/blob/main/docs/1_installation.md
+1. https://github.com/pay-rails/pay/blob/main/docs/stripe/8_stripe_checkout.md#how-to-use-stripe-checkout-with-pay
+1. https://github.com/pay-rails/pay/blob/main/docs/7_webhooks.md
+1. https://github.com/pay-rails/pay/blob/main/docs/stripe/5_webhooks.md
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,98 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_220536) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_222045) do
+  create_table "pay_charges", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "subscription_id"
+    t.string "processor_id", null: false
+    t.integer "amount", null: false
+    t.string "currency"
+    t.integer "application_fee_amount"
+    t.integer "amount_refunded"
+    t.json "metadata"
+    t.json "data"
+    t.string "stripe_account"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "processor_id"], name: "index_pay_charges_on_customer_id_and_processor_id", unique: true
+    t.index ["subscription_id"], name: "index_pay_charges_on_subscription_id"
+  end
+
+  create_table "pay_customers", force: :cascade do |t|
+    t.string "owner_type"
+    t.bigint "owner_id"
+    t.string "processor", null: false
+    t.string "processor_id"
+    t.boolean "default"
+    t.json "data"
+    t.string "stripe_account"
+    t.datetime "deleted_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_type", "owner_id", "deleted_at"], name: "pay_customer_owner_index", unique: true
+    t.index ["processor", "processor_id"], name: "index_pay_customers_on_processor_and_processor_id", unique: true
+  end
+
+  create_table "pay_merchants", force: :cascade do |t|
+    t.string "owner_type"
+    t.bigint "owner_id"
+    t.string "processor", null: false
+    t.string "processor_id"
+    t.boolean "default"
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_type", "owner_id", "processor"], name: "index_pay_merchants_on_owner_type_and_owner_id_and_processor"
+  end
+
+  create_table "pay_payment_methods", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "processor_id", null: false
+    t.boolean "default"
+    t.string "type"
+    t.json "data"
+    t.string "stripe_account"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "processor_id"], name: "index_pay_payment_methods_on_customer_id_and_processor_id", unique: true
+  end
+
+  create_table "pay_subscriptions", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "name", null: false
+    t.string "processor_id", null: false
+    t.string "processor_plan", null: false
+    t.integer "quantity", default: 1, null: false
+    t.string "status", null: false
+    t.datetime "current_period_start", precision: nil
+    t.datetime "current_period_end", precision: nil
+    t.datetime "trial_ends_at", precision: nil
+    t.datetime "ends_at", precision: nil
+    t.boolean "metered"
+    t.string "pause_behavior"
+    t.datetime "pause_starts_at", precision: nil
+    t.datetime "pause_resumes_at", precision: nil
+    t.decimal "application_fee_percent", precision: 8, scale: 2
+    t.json "metadata"
+    t.json "data"
+    t.string "stripe_account"
+    t.string "payment_method_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "processor_id"], name: "index_pay_subscriptions_on_customer_id_and_processor_id", unique: true
+    t.index ["metered"], name: "index_pay_subscriptions_on_metered"
+    t.index ["pause_starts_at"], name: "index_pay_subscriptions_on_pause_starts_at"
+  end
+
+  create_table "pay_webhooks", force: :cascade do |t|
+    t.string "processor"
+    t.string "event_type"
+    t.json "event"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,4 +109,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_220536) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pay_charges", "pay_customers", column: "customer_id"
+  add_foreign_key "pay_charges", "pay_subscriptions", column: "subscription_id"
+  add_foreign_key "pay_payment_methods", "pay_customers", column: "customer_id"
+  add_foreign_key "pay_subscriptions", "pay_customers", column: "customer_id"
 end
