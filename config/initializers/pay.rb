959dd9383468df5a14d99b0bc58bc7de6828f561
Sheheryar Sewani <shey@shey.ca>
2024-05-14 15:43:08 -0400

    Stripe Events and Postgresql

--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -1,5 +1,9 @@
 Pay.setup do |config|
-  config.support_email    = "happy@acme.test"
+  config.support_email = "happy@acme.test"
   config.application_name = "demo"
-  config.business_name    = "demo"
+  config.business_name = "demo"
+end
+
+ActiveSupport.on_load(:pay) do
+  Pay::Webhooks.delegator.subscribe "stripe.invoice.payment_succeeded", PaymentSucceededHandler.new
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_13_222045) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pay_charges", force: :cascade do |t|
     t.bigint "customer_id", null: false
     t.bigint "subscription_id"
