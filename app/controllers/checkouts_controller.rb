3afa76d60b95b1883ec38a72ed740f061c96f635
Sheheryar Sewani <shey@shey.ca>
2024-05-13 22:52:57 -0400

    Redirect to Stripe (#5)

---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,32 @@
+class CheckoutsController  < ApplicationController
+  before_action :set_price_id, only: %i[show]
+
+  def show
+    redirect_to stripe_checkout_url, allow_other_host: true, status: :see_other
+  end
+
+  private
+  attr_reader :user
+
+  def stripe_checkout_url
+    StripeCheckout.new(user, @stripe_price_id).url
+  end
+
+  def user
+    User.first
+  end
+
+  def set_price_id
+    plan_name = params[:plan]
+    @stripe_price_id = plan_id_mapping[plan_name]
+  end
+
+  def plan_id_mapping
+    {
+      "hobby" => "price_1Ow66SAWnWpxHPD5qEZoUi4O",
+      "professional" => "price_1Ow66SAWnWpxHPD5K270wTji",
+      "enterprise" => "price_1Ow66RAWnWpxHPD5xV9xUCyD"
+    }
+  end
+
+end
---
+++ app/services/stripe_checkout.rb
@@ -0,0 +1,29 @@
+class StripeCheckout
+  include Rails.application.routes.url_helpers
+
+  attr_reader :user, :stripe_price_id
+  def initialize(user, stripe_price_id)
+    @user = user
+    @stripe_price_id = stripe_price_id
+  end
+
+  def success_url
+    root_url
+  end
+
+  def url
+    checkout.url
+  end
+
+  private
+
+  def checkout
+    user.payment_processor.checkout(
+      mode: "subscription",
+      line_items: stripe_price_id,
+      success_url: success_url,
+      billing_address_collection: "auto",
+      allow_promotion_codes: false,
+    )
+  end
+end
